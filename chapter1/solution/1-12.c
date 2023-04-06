bbe70db61ac198b2a8e3582ec96a806c384feb59
jaweee <3466473208@qq.com>
2023-04-06 15:36:50 +0800

    write input line by line splited by word successfully

--- chapter1/solution/1-12.c
+++ chapter1/solution/1-12.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
+#define MAXLINE 1000
+#define OUT = 0
+#define IN = 1
+
 int main() {
-    int c = 0;
-    while ((c = getchar()) != 0){
-        if () 
+    char ret_buf[MAXLINE];
+    int c, len, once;
+    c = len = 0;
+    once = 1;
+    while ((c = getchar()) != EOF) {
+        /*if (c == '\t' || c == '\n' || c == ' ' && once == 1){
+            c = '\n';
+            ret_buf[len++] = c;
+            once = 0;
+            continue;
+        }
+        else if (c == '\t' || c == '\n' || c == ' ' && once == 0) {
+            continue;
+        }*/
+        if (c == '\t' || c == '\n' || c == ' ') {
+            if (once == 1) {
+                c = '\n';
+                ret_buf[len++] = c;
+                once = 0;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            once = 1;
+            ret_buf[len++] = c;
+        }
     }
+    ret_buf[len] = '\0';
+    printf("\nshift line string is flowing behind\n%s\n", ret_buf);
 }
